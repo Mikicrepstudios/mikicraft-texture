49e765ce53fce116e31e2c9e4ca256b4c812f9af
Mikicrep <mikicrepoffical@gmail.com>
2024-09-22 11:20:07 +0200

    Fixed grass block

--- update.sh
+++ update.sh
@@ -1,2 +1,10 @@
-sudo cp . ~/.minecraft/resourcepacks/mikicraft-texture -r
+sudo rm /home/mikicrep/.minecraft/resourcepacks/mikicraft-texture -rf
+sudo cp . /home/mikicrep/.minecraft/resourcepacks/mikicraft-texture -r
 
+sudo chmod 777 /home/mikicrep/.minecraft/resourcepacks/mikicraft-texture/*
+sudo chmod 777 /home/mikicrep/.minecraft/resourcepacks/mikicraft-texture/*/*
+sudo chmod 777 /home/mikicrep/.minecraft/resourcepacks/mikicraft-texture/*/*/*
+sudo chmod 777 /home/mikicrep/.minecraft/resourcepacks/mikicraft-texture/*/*/*/*
+sudo chmod 777 /home/mikicrep/.minecraft/resourcepacks/mikicraft-texture/*/*/*/*/*
+sudo chmod 777 /home/mikicrep/.minecraft/resourcepacks/mikicraft-texture/*/*/*/*/*/*
+sudo chmod 777 /home/mikicrep/.minecraft/resourcepacks/mikicraft-texture/*/*/*/*/*/*/*
